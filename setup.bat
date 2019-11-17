42fb6b6cd9329e0a798175e5765720f0f5a8a394
Bernd Engels <engels@goldenacker.de>
2019-11-17 17:34:30 +0100

    setup for windows

--- setup.bat
+++ setup.bat
@@ -1,21 +1,24 @@
 @echo OFF
-composer install
-npm install
-npm run dev
 
-if(![System.IO.File]::Exists(".env")) {
-    echo "create .env file`n"
-    Copy-Item ".env.local" -Destination ".env"
-}
-if(![System.IO.File]::Exists(".htaccess")) {
-    echo "create .htaccess file`n"
-    Copy-Item "htaccess-tpl" -Destination ".htaccess"
-}
+echo install composer/node_modules packages
+call composer install
+call npm install
+call npm run dev
 
-echo "clear all caches`n"
+if NOT exist .env (
+    echo create .env file
+    copy .env.local .env
+)
+if NOT exist .htaccess (
+    echo create .htaccess file
+    copy htaccess-tpl .htaccess
+)
+
+echo clear all caches
 php artisan cache:clear
 php artisan config:clear
 php artisan route:clear
 php artisan view:clear
-echo "all DONE :-)`n"
-echo "Dont forget to make entries for the hostname in C:\Window\System32\drivers\etc\host`nand in your apache httpd-vhosts.conf file`n"
+echo all DONE :-)
+echo Dont forget to make entries for the hostname in C:\Window\System32\drivers\etc\host
+echo and in your apache httpd-vhosts.conf file
