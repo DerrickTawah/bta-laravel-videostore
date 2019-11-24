a601f9c4cf252035631cf74f3645cc720c27ffed
Bernd Engels <engels@goldenacker.de>
2019-11-24 22:22:47 +0100

    bug fix

--- app/Http/Controllers/API/ApiMovieController.php
+++ app/Http/Controllers/API/ApiMovieController.php
@@ -48,7 +48,7 @@ class ApiMovieController extends Controller
      */
     public function show($id)
     {
-        $movie = Movie::whereId($id)->get();
+        $movie = Movie::whereId($id)->first();
         return response()->json(compact('movie'));
     }
 
